6021e1e999f98cc13f46330d998d1cde0d74865f
Quinn McPhail <quinnmcphail@me.com>
2019-05-29 16:58:10 -0400

    Got session information

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,3 +1,40 @@
-import { LightningElement } from 'lwc';
+/* global chrome*/
+import { LightningElement, track } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    @track loaded = true;
+    @track sfHost;
+    @track session;
+
+    connectedCallback() {
+        const sfHostReturn = new Promise((resolve, reject) => {
+            chrome.runtime.sendMessage({ message: 'getSfHost' }, sfHost => {
+                if (sfHost) {
+                    resolve(sfHost);
+                }
+                reject('Error retrieving the current hostname.');
+            });
+        });
+        const sessionReturn = hostname =>
+            new Promise((resolve, reject) => {
+                chrome.runtime.sendMessage(
+                    { message: 'getSession', sfHost: hostname },
+                    session => {
+                        if (session) {
+                            resolve(session);
+                        }
+                        reject('Error retrieving the current session.');
+                    }
+                );
+            });
+        sfHostReturn
+            .then(hostname => {
+                this.sfHost = hostname;
+                return sessionReturn(hostname);
+            })
+            .then(session => {
+                this.session = JSON.stringify(session);
+                this.loaded = true;
+            });
+    }
+}
